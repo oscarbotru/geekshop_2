4426261e6ab7fa91d23c939c8691348795eef465
olegmaslov <oleg.gnome@yandex.ru>
2022-01-14 20:17:32 +0300

    remove wrong files

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -5,9 +5,11 @@ from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, UpdateView, CreateView, DetailView, DeleteView
 from django.dispatch import receiver
 from basketapp.models import Basket
+from mainapp.models import Product
 from ordersapp.forms import OrderItemForm
 from ordersapp.models import Order, OrderItem
 from django.db.models.signals import pre_save, pre_delete
+from django.http import JsonResponse
 
 
 class OrderListView(ListView):
@@ -133,3 +135,13 @@ def product_quantity_update_on_save(sender, update_fields, instance, **kwargs):
 def product_quantity_update_on_delete(sender, instance, **kwargs):
     instance.product.quantity += instance.quantity
     instance.product.save()
+
+
+def get_product_price(request, pk):
+    product_price = 0.0
+
+    product = Product.objects.filter(pk=pk, is_active=True).first()
+    if product:
+        product_price = product.price
+
+    return JsonResponse({'price': product_price})
